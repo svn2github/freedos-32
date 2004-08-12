c4c4d6e4fd1a5befb0a20f7ff9f7cbbefa6992a1
lucabe <lucabe@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2004-08-12 13:39:30 +0000

    - read() changed its syntax...
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@172 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- tests/read.c
+++ tests/read.c
@@ -9,7 +9,7 @@ void _init(void)
   while(!done) {
     res = 0;
     while (res == 0) {
-      res = keyb_read(0, &c, 0);
+      res = keyb_read(0, 0, &c);
     }
     message("U pressed %c\n", c);
     if (c == 'q') {
@@ -22,7 +22,7 @@ void _init(void)
   while(!done) {
     res = 0;
     message("U pressed... ");
-    res = keyb_read(0, &c, 1);
+    res = keyb_read(0, 1, &c);
     message("%c!!!\n", c);
     if (c == 'q') {
       message("Ok, now testing n-char blocking read...\n");
@@ -37,7 +37,7 @@ void _init(void)
     c1[5] = 0;
     res = 0;
     message("U pressed... ");
-    res = keyb_read(0, c1, 5);
+    res = keyb_read(0, 5, c1);
     message("%s!!!\n", c1);
     if (c1[0] == 'q') {
       message("G-Bye!!!\n");
