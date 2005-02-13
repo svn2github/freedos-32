a52f8b3d3c2031c4f51c4b9ef2895090d9eba613
lucabe <lucabe@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-02-13 15:09:54 +0000

    Change file namings: linux.mk	---> Compile under Linux linux_djgpp.mk	---> Cross-compile under linux using DJGPP linux_mingw.mk	---> Cross-compile under linux using MinGW djgpp.mk	---> Compile under DOS using DJGPP mingw.mk	---> Compile under Windows using MinGW
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@271 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- fd32/mk/linux.mk
+++ fd32/mk/linux.mk
@@ -1,15 +1,15 @@
-CC  = i586-pc-msdosdjgpp-gcc
-AS  = i586-pc-msdosdjgpp-gcc
-LD  = i586-pc-msdosdjgpp-ld
-AR  = i586-pc-msdosdjgpp-ar
+CC  = -gcc
+AS  = gcc
+LD  = ld
+AR  = ar
 INCL   = $(BASE)
 VMINCL  = -I$(BASE)/include/i386 -I$(BASE)/include -I$(BASE)/include/sys/ll
 LIB_PATH    = $(BASE)/lib/
 LIB_DIR  = $(BASE)/lib
 
-C_OPT =  -Wall -O -finline-functions -fno-builtin -nostdinc -D__GNU__ -I$(INCL)
-ASM_OPT =  -x assembler-with-cpp -D__GNU__ -I$(INCL)
-LINK_OPT = -T $(OSLIB)/mk/os.x -Bstatic -Ttext 0x100000 -nostartfiles -nostdlib -L$(LIB_PATH)
+C_OPT =  -Wall -O -finline-functions -fno-builtin -nostdinc -D__LINUX__ -I$(INCL)
+ASM_OPT =  -x assembler-with-cpp -D__LINUX__ -I$(INCL)
+LINK_OPT = -Bstatic -Ttext 0x100000 -s -nostartfiles -nostdlib -L$(LIB_PATH)
 
 MKDIR	= mkdir
 CP	= cp
--- fd32/mk/linux_mingw.mk
+++ fd32/mk/linux_mingw.mk
@@ -1,7 +1,7 @@
-CC  = mingw32-gcc
-AS  = mingw32-gcc
-LD  = ld
-AR  = ar
+CC  = i386-mingw32-gcc
+AS  = i386-mingw32-gcc
+LD  = i386-mingw32-ld
+AR  = i386-mingw32-ar
 
 INCL   = $(BASE)
 VMINCL  = -I$(BASE)/include/i386 -I$(BASE)/include -I$(BASE)/include/sys/ll
@@ -12,11 +12,11 @@ C_OPT =  -Wall -O -finline-functions -nostdinc -mno-stack-arg-probe -ffreestandi
 ASM_OPT =  -x assembler-with-cpp -D__WIN32__ -I$(INCL)
 LINK_OPT = -T $(OSLIB)/mk/os-mingw.x -Bstatic -Ttext 0x120000 -nostartfiles -nostdlib -L$(LIB_PATH)
 
-MKDIR	= md
-CP	= copy
-CAT	= @type
-RM	= del
-RMDIR	= rd
+MKDIR	= mkdir
+CP	= cp
+CAT	= cat
+RM	= rm -rf
+RMDIR	= rm -rf
 
 # Common rules
 %.o : %.s
