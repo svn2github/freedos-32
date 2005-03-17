79845ebe96f511fbef96905c620c9d518b3e26e5
lucabe <lucabe@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-03-17 13:11:13 +0000

    fix sbrk for negative increments
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@316 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- apps/libfd32/src/sbrk.c
+++ apps/libfd32/src/sbrk.c
@@ -13,13 +13,18 @@ void *sbrk(int incr)
   extern DWORD mem_limit;
 
   if (mem_limit != 0) {
-    res = mem_get_region(mem_limit, incr);
-    if (res == -1) {
-      message("Ermmm... SBRK problem: cannot memget(%lx %x)\n", mem_limit, incr);
-      mem_dump();
-      return 0;
-    }
     prev_heap_end = (char *)mem_limit;
+    if (incr > 0) {
+      res = mem_get_region(mem_limit, incr);
+      if (res == -1) {
+        message("Ermmm... SBRK problem: cannot memget(%lx %x)\n",
+			mem_limit, incr);
+        mem_dump();
+        return 0;
+      }
+    } else if (incr < 0) {
+      mem_free(mem_limit + incr, incr);
+    }
     mem_limit += incr;
   } else {
     message("sbrk error: Memory Limit == 0!\n");
