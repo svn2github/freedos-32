35aca648b30f7522a47c868f0c25177c3e56905a
nlabugt <nlabugt@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-05-04 09:49:21 +0000

    Copying syscall header from drivers/include. For filesys header se dev list.
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@409 85e9c19b-ca11-0410-bd92-afdf7feba87b

---
+++ apps/newlib-port/sys/fd32/sys/syscalls.h
@@ -0,0 +1,24 @@
+#ifndef __FD32_CALLS__
+#define __FD32_CALLS__
+/* Prototypes of the FD32 kernel calls... */
+
+int  message(char *fmt,...) __attribute__((format(printf,1,2)));
+void fd32_abort(void);
+void restore_sp(int res);
+int mem_get_region(DWORD base, DWORD size);
+DWORD mem_get(DWORD amount);
+int mem_free(DWORD base, DWORD size);
+void IDT_place(BYTE num,void (*handler)(void));
+int fd32_allocate_descriptors(WORD NumSelectors, WORD *BaseSelector);
+int fd32_free_descriptor(WORD Selector);
+WORD fd32_get_selector_increment_value();
+int fd32_get_segment_base_address(WORD Selector, DWORD *BaseAddress);
+int fd32_set_segment_base_address(WORD Selector, DWORD BaseAddress);
+int fd32_set_segment_limit(WORD Selector, DWORD Limit);
+int fd32_set_descriptor_access_rights(WORD Selector, WORD Rights);
+int fd32_create_alias_descriptor(WORD Selector, WORD *NewSelector);
+int fd32_get_descriptor(WORD Selector, WORD BufferSelector, DWORD BufferOffset);
+int fd32_set_descriptor(WORD Selector, WORD BufferSelector, DWORD BufferOffset);
+DWORD GDT_read(WORD sel,DWORD *lim,BYTE *acc,BYTE *gran);
+int vm86_callBIOS(int service,X_REGS16 *in,X_REGS16 *out,X_SREGS16 *s);
+#endif
