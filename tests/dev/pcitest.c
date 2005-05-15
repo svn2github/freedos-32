2027740df1fd0418c185aa99c9b2c35de2ff3d40
hanzac <hanzac@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-05-15 00:55:17 +0000

    Using a general Makefile in tests/dev and create a pcitest
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@434 85e9c19b-ca11-0410-bd92-afdf7feba87b

---
+++ tests/dev/pcitest.c
@@ -0,0 +1,31 @@
+#include <dr-env.h>
+#include <errors.h>
+#include <devices.h>
+#include <stubinfo.h>
+
+#include "../../drivers/pci/pci.h"
+
+int pcitest_init(int argc, char *argv[])
+{
+  int hdev, devid;
+  fd32_request_t *pci_request;
+  pci_request_t pcirq;
+  pci_device *pcidev;
+  pcirq.size = sizeof(pci_request_t);
+  pcirq.base_class = 0x01;
+  pcirq.sub_class = 0x01;
+  pcirq.pcidev_obj = &pcidev;
+  
+  hdev = fd32_dev_search("pci");
+  if (hdev >= 0 && fd32_dev_get(hdev, &pci_request, NULL, NULL, 0) >= 0)
+  {
+    if ((devid = pci_request(FD32_PCI_GET_DEVICE, &pcirq)) >= 0)
+    {
+    /* read PCI config */
+      fd32_message("vender: %d\n", pcidev->pci_dv_vendor);
+    /* write PCI config */
+      
+    }
+  }
+  return 0;
+}
