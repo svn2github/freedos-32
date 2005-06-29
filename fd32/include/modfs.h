702ce405fd32906d76b2dcffebd074c9de8bc830
hanzac <hanzac@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-06-29 13:33:30 +0000

    Modules number is becoming unlimited, using a pseudo modfs_open when loading the next module
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@485 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- fd32/include/modfs.h
+++ fd32/include/modfs.h
@@ -8,6 +8,7 @@
 #define __MODFS_H__
 
 void modfs_init(struct kern_funcs *kf, DWORD addr, int count);
+void modfs_open(DWORD addr, int mod_index);
 int modfs_offset(int file, int offset);
 
 #endif /* __MODFS_H__ */
