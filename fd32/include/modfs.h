23acd724676364154d684dc67f230640e1d34f26
hanzac <hanzac@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-07-28 08:14:27 +0000

    Simplify and generalize the module loading
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@510 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- fd32/include/modfs.h
+++ fd32/include/modfs.h
@@ -7,6 +7,8 @@
 #ifndef __MODFS_H__
 #define __MODFS_H__
 
+#include "format.h"
+
 void modfs_init(struct kern_funcs *kf, DWORD addr, int count);
 void modfs_open(DWORD addr, int mod_index);
 int modfs_offset(int file, int offset);
--- fd32/include/mods.h
+++ fd32/include/mods.h
@@ -3,6 +3,8 @@
  * 
  * This is free software; see GPL.txt
  */
+
 void process_modules(int mods_count, DWORD mods_addr);
 void module_address(DWORD mods_addr, int i, DWORD *start, DWORD *end);
 char *module_cl(DWORD mods_addr, int i);
+void mem_release_module(DWORD mstart, int m, int mnum);
