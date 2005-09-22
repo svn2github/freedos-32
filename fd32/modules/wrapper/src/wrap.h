9589a2727caeae87a8f48ad9030f10aea64e2b22
hanzac <hanzac@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-09-22 14:02:40 +0000

    * Rename doshdr.h to wrap.h and add some definitions
    !Forgot the following log in the previous update of (wrap.c, private_funcs.c)!
    * Replace mem_get_region with mem_get, because dj-libc doesn't need the memory address to be above the image base (based on the related talk on the mail-list)
    
    
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@565 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- fd32/modules/wrapper/src/wrap.h
+++ fd32/modules/wrapper/src/wrap.h
@@ -4,6 +4,24 @@
  * This is free software; see GPL.txt
  */
 
+#ifndef __WRAP_H__
+#define __WRAP_H__
+
+#include <ll/i386/hw-data.h>
+#include "kernel.h"
+
+#define VERSION "0.1"
+#define RUN_RING 0
+
+extern WORD user_cs, user_ds;
+
+/* from dpmi/src/dos_exec.c, using dynamic linking */
+WORD stubinfo_init(DWORD base, DWORD initial_size, DWORD mem_handle, char *filename, char *args);
+void restore_psp(void);
+
+/* similar with fd32/kernel/exec.c */
+int my_exec_process(struct kern_funcs *p, int file, struct read_funcs *parser, char *filename, char *args);
+
 typedef struct dos_header {
   WORD e_magic;		/* Magic number 			*/
   WORD e_cblp;		/* Bytes on last page of file		*/
@@ -28,3 +46,25 @@ typedef struct dos_header {
   WORD e_res2[10];	/* Reserved words			*/
   DWORD e_lfanew;	/* File address of new exe header	*/
 } __attribute__ ((packed)) tDOSHD;
+
+/* NOTE: Move the structure here, Correct? */
+struct stubinfo {
+  char magic[16];
+  DWORD size;
+  DWORD minstack;
+  DWORD memory_handle;
+  DWORD initial_size;
+  WORD minkeep;
+  WORD ds_selector;
+  WORD ds_segment;
+  WORD psp_selector;
+  WORD cs_selector;
+  WORD env_size;
+  char basename[8];
+  char argv0[16];
+  char dpmi_server[16];
+  /* FD/32 items */
+  DWORD dosbuf_handler;
+};
+
+#endif
