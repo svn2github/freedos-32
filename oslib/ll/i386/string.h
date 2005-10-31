6ebe6dbed07c8a15c0372bf7219b38091a4f7e1f
hanzac <hanzac@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-10-31 01:47:14 +0000

    Remove some unused declaration
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@629 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- oslib/ll/i386/string.h
+++ oslib/ll/i386/string.h
@@ -29,27 +29,9 @@
 #include <ll/i386/defs.h>
 BEGIN_DEF
 
-#if 0
 /* Various string manipulation functions */
 
 /* File: string.c		*/
-
-char *strcpy(char *dst,const char *src);
-char *strncpy(char *dst,const char *src,int n);
-int strcmp(const char *s1,const char *s2);
-int strncmp(const char *s1,const char *s2,int n);
-int strlen(const char *s);
-char *strscn(char *s,char *pattern);
-char *strchr(char *s,int c);
-char *strupr(char *s);
-char *strlwr(char *s);
-char *strcat(char *dst,char *src);
-
-/* File: strstr.c */
-
-char *strstr(const char *haystack, const char *needle);
-
-#endif
 
 char *strscn(char *s,char *pattern);
 char *strupr(char *s);
