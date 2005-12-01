2697f7c3410d7ee912f69fdff17084b6a6dade69
hanzac <hanzac@85e9c19b-ca11-0410-bd92-afdf7feba87b>
2005-12-01 00:55:39 +0000

    ...
    git-svn-id: https://svn.code.sf.net/p/freedos-32/code/trunk@674 85e9c19b-ca11-0410-bd92-afdf7feba87b

--- fd32/boot/mmap.h
+++ fd32/boot/mmap.h
@@ -4,5 +4,5 @@
  * This is free software; see GPL.txt
  */
 
-uint32_t mmap_entry(uint32_t addr, uint32_t *bah, uint32_t *bal, uint32_t *lh, uint32_t *ll, uint32_t *type);
-void mmap_process(uint32_t addr, uint32_t len, uint32_t *lb, uint32_t *ls, uint32_t *hb, uint32_t *hs);
+DWORD mmap_entry(DWORD addr, DWORD *bah, DWORD *bal, DWORD *lh, DWORD *ll, DWORD *type);
+void mmap_process(DWORD addr, DWORD len, DWORD *lb, DWORD *ls, DWORD *hb, DWORD *hs);
